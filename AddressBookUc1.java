6003e885c21f67269da3986b6d79b400cebb9a4a
Srinivas1130 <srinivasachar1998@gmail.com>
2022-06-10 18:43:06 +0530

    Added code for Address Book

---
+++ AddressBookUc1.java
@@ -0,0 +1,49 @@
+package com.bridgelabz.day24;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Scanner;
+
+public class AddressBookUc1 {
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String state;
+	private int zip;
+	private long phoneNumber;
+	private String eMail;
+	
+	AddressBookUc1(String firstName, String lastName, String address, String state, int zip, long phoneNumber, String eMail ){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+		this.eMail = eMail;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public String getState() {
+		return state;
+	}
+	public int getZip() {
+		return zip;
+	}
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+	public String getEMail() {
+		return eMail;
+	}
+	public String toString() {
+		return "First Name : "+firstName+" "+"Last Name : "+lastName+" "+"Address : "+address+" "+"State : "+state+" "+"Zip : "+zip+" "+"Phone Number : "+phoneNumber+" "+"Email : "+eMail;
+	}
+}
